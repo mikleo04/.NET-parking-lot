f9fb8ea46219fc2300cd681f97dad975f350fe60
Hafid Siraj Aurakhmah Witra <hafidsiraj.aw@gmail.com>
2024-04-18 06:49:34 +0700

    [IMPROVE] instantiated view menu in entry point class

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
-﻿
+﻿using parking_lot.view;
+
 namespace parking_lot
 {
     class Program
     {
         private static void Main(string[] args)
         {
-
+            Menu menu = new Menu();
+            menu.Run();
         }
     }
 }
